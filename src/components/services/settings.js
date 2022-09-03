64af0d411dcca32d7e4d538ac16b4cd6d7b710bb
Stepan Kryukov <stephenkryukov@gmail.com>
2022-09-03 22:24:01 +0300

    commit

---
+++ src/components/services/settings.js
@@ -0,0 +1,8 @@
+const settings = {
+  BASE_URL: 'https://pixabay.com/api/?',
+  API_KEY: 'key=28702547-e6a50c7f62eac093665197ede',
+  TYPE: '&image_type=photo',
+  PER_PAGE: '&per_page=12',
+};
+
+export default settings;
